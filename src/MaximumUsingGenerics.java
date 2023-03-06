b0f6f8b671066a0e9026a244cb288f962b27cf53
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-06 16:36:34 +0530

    find maximum number

--- src/MaximumUsingGenerics.java
+++ src/MaximumUsingGenerics.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class MaximumUsingGenerics {
-    public static void main(String[] args) {
+    public static <new_data> void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter 3 Number :");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        maxNum(a,b,c);
+    }
+
+   public static void maxNum(int a , int b,int c){
+        if (a>b && a>c){                                 //a.compareTo(b) not working
+            System.out.println(a+" is greater");
+        }else if (b>c){
+            System.out.println(b+" is greater");
+        }else {
+            System.out.println(c+" is graeter");
+        }
 
     }
 }
